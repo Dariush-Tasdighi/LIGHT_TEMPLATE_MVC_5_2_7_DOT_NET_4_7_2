6204f21ed80c6eba1c3a11e4c39aa56dd4682764
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2019-01-13 14:45:08 +0330

    Add project files.

---
+++ MY_WEB_APPLICATION/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿//using System.Linq;
+
+namespace MY_WEB_APPLICATION.Controllers
+{
+	public class HomeController : System.Web.Mvc.Controller
+	{
+		public HomeController() : base()
+		{
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public System.Web.Mvc.ViewResult Index()
+		{
+			return View();
+		}
+	}
+}
